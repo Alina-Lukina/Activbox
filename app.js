bcbc537dfe3469809eb9cb75d7e3fb0687f54e56
Alina Lukina <alina.lukina.bfu@gmail.com>
2020-02-16 22:53:22 +0200

    js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-$(function() {
+$(function () {
 
-   let header = $("#header"); /* объявили переменную хедер коt = селекtору хедер */
+    let header = $("#header"); /* объявили переменную хедер коt = селекtору хедер */
     let intro = $("#intro");
     let introH = intro.innerHeight(); /* высоtа блока инtро, вызываем меtод хейд */
     let scrollPos = $(window).scrollTop(); /* позиция скрола */
 
     $(windows).on("scroll", function() {
-
         scrollPos = $(this).scrollTop();
+        if(scrollPos > introH) {
+            header.addClass("fixed"); /* придали класс хедеру fixed */
+        } else {
+            header.removeClass("fixed");
+        }
         console.log(scrollPos);
-
 
     });
 
 
-    header.addClass("fixed"); /* придали класс хедеру fixed */
+
 });
