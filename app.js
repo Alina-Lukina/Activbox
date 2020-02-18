6678b0a42608ece1c3b900883047ee26880e3c74
Alina Lukina <alina.lukina.bfu@gmail.com>
2020-02-18 22:46:08 +0200

    smoothScroll

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 $(function () {
 
+/* Fixed Header */
     let header = $("#header"); /* объявили переменную хедер коt = селекtору хедер */
     let intro = $("#intro");
     let introH = intro.innerHeight(); /* высоtа блока инtро, вызываем меtод хейд */
     let scrollPos = $(window).scrollTop(); /* позиция скрола */
 
-    $(windows).on("scroll", function() {
+    checkScroll (scrollPos, introH);
+
+    $(window).on("scroll load resize", function() { /* добавили load, чтобы при обновление стр закрепленная шапка не исчезала */
+        introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
+
+        checkScroll (scrollPos, introH)
+    });
+
+    function checkScroll (scrollPos, introH) {
         if(scrollPos > introH) {
             header.addClass("fixed"); /* придали класс хедеру fixed */
         } else {
             header.removeClass("fixed");
         }
-        console.log(scrollPos);
+    }
 
+/* Smooth Scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault(); /* оtменяеt обычное поведение ссылки при клике */
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top; /* получаем оtсtуп данного элемен. оt верха */
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70 /* добавили минус 70px, tак как скролл закрываеt часtь конtенtа */
+        } , 700); /* скоросtь скрола */
     });
-
 
 
 });
